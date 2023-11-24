3308b4f2772e52713713fbc15059570d0e2fc459
Salomao-Faleta <faletas41@gmail.com>
2023-11-23 23:47:37 -0300

    Fazendo o modal

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const modal = document.querySelector('.modal');
+const buttonReset = document.querySelector('#reset');
 
+const jump = () => {
+    mario.classList.add('jump'); /*Adiciona pulo*/
 
-const jump = ()=>{
-    mario.classList.add('jump'); /*Adiciona pulo*/ 
-
-    setTimeout(()=>{ /*Espera um tempo e remove a class de pulo*/
-        mario.classList.remove('jump');  
+    setTimeout(() => { /*Espera um tempo e remove a class de pulo*/
+        mario.classList.remove('jump');
     }, 500)
 
 }
 
 
-const loop = setInterval(()=>{
+const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); // (+) transforma o texto em número
     const cloudsPosiiton = +window.getComputedStyle(clouds).right.replace('px', '');;
 
 
+    //SE O MARIO ENCOSTA NO TUBO, PARE O JOGO 
+    if (pipePosition <= 165 && pipePosition > 0 && marioPosition < 100) {
 
-    if(pipePosition <= 165 && pipePosition > 0 && marioPosition < 100){
+        //amostrando o modal
+        modal.style.display = 'block';
+
+
+        //parando o pipe()tubo
         pipe.style.animation = 'nome';
         pipe.style.left = `${pipePosition}px`;
 
+        //trocando a imagem do Mario
         mario.style.animation = 'nome';
         mario.style.bottom = `${marioPosition}px`;
         mario.src = './imagens/game-over.png';
@@ -32,7 +40,7 @@ const loop = setInterval(()=>{
         mario.style.marginLeft = '80px';
 
 
-
+        //parando as nuvens
         clouds.style.animation = 'nome';
         clouds.style.right = `${cloudsPosiiton}px`;
 
@@ -41,6 +49,17 @@ const loop = setInterval(()=>{
 
 }, 10)
 
+
+//fechar o modal
+buttonReset.addEventListener('click', () => {
+    //escondendo o modal
+    modal.style.display = 'none';
+
+
+   
+
+
+})
 
 
 
